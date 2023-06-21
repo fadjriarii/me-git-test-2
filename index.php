9876bd9581cd91fbdd81ca80feae4f9f8ceae0a8
Fadjriarii <17210999@bsi.ac.id>
2023-06-21 14:27:17 +0700

    menambahkan paragraf pada index.php

--- index.php
+++ index.php
@@ -10,5 +10,7 @@
 </head>
 <body>
     <h1>Hello World</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit rem sed modi velit nihil voluptate dicta minus, exercitationem aspernatur ea accusamus ducimus? Sint eius quibusdam nam ab voluptatum ipsam quaerat!
+    </p>
 </body>
 </html>
